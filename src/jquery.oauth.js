5e32cf1a81bb86fa038046a7bbd9b80e5cd71bc2
Esben Petersen <esbenspetersen@gmail.com>
2015-01-05 21:50:01 +0100

    Setup base module structure

---
+++ src/jquery.oauth.js
@@ -0,0 +1,15 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(this, function ($) {
+
+
+    return {
+
+    };
+}));
